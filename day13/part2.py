25e737b3bea5e8d7c072938bf07d58297bda4176
Cameron Dale <camrdale@gmail.com>
2024-12-13 10:26:42 -0800

    Day 13

---
+++ day13/part2.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+from pathlib import Path
+
+from shared import parse, win
+
+INPUT_FILE = Path(__file__).parent.resolve() / 'input.txt'
+TEST_INPUT = """
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+"""
+
+
+def main():
+    with INPUT_FILE.open() as ifp:
+        machines = parse(
+                # TEST_INPUT.split('\n')
+                ifp.readlines()
+            )
+
+    machines = [
+        machine._replace(x=machine.x+10000000000000, y=machine.y+10000000000000)
+        for machine in machines]
+
+    total_tokens = 0
+    wins = 0
+    for machine in machines:
+        tokens = win(machine)
+        if tokens is not None:
+            total_tokens += tokens
+            wins += 1
+
+    print('Total tokens to win', wins, 'prizes:', total_tokens)
+
+
+if __name__ == '__main__':
+    main()
---
+++ day13/shared.py
@@ -0,0 +1,55 @@
+import re
+from collections.abc import Iterable
+from typing import NamedTuple
+
+
+BUTTON_A = re.compile(r'Button A: X\+([0-9]*), Y\+([0-9]*)')
+BUTTON_B = re.compile(r'Button B: X\+([0-9]*), Y\+([0-9]*)')
+PRIZE = re.compile(r'Prize: X\=([0-9]*), Y\=([0-9]*)')
+
+
+class Machine(NamedTuple):
+    xa: int
+    ya: int
+    xb: int
+    yb: int
+    x: int
+    y: int
+
+
+def parse(input: Iterable[str]) -> list[Machine]:
+    machines: list[Machine] = []
+    xa = 0
+    xb = 0
+    x = 0
+    ya = 0
+    yb = 0
+    y = 0
+    for line in input:
+        if line.strip() == '':
+            continue
+        if match := BUTTON_A.match(line):
+            xa, ya = int(match.group(1)), int(match.group(2))
+        elif match := BUTTON_B.match(line):
+            xb, yb = int(match.group(1)), int(match.group(2))
+        elif match := PRIZE.match(line):
+            x, y = int(match.group(1)), int(match.group(2))
+            machines.append(Machine(xa, ya, xb, yb, x, y))
+    return machines
+
+
+def win(m: Machine) -> int | None:
+    b_num = m.y*m.xa - m.x*m.ya
+    b_denom = m.xa*m.yb - m.xb*m.ya
+    if b_num % b_denom != 0:
+        return None
+    b = b_num // b_denom
+
+    a_num = m.y*m.xb - m.x*m.yb
+    a_denom = m.xb*m.ya - m.xa*m.yb
+    if a_num % a_denom != 0:
+        return None
+    a = a_num // a_denom
+
+    # print(f'Optimal solution: {a}*{m.xa} + {b}*{m.xb} = {m.x}, {a}*{m.ya} + {b}*{m.yb} = {m.y}')
+    return a*3 + b
