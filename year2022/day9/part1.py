d70adc6319812ec48129c3d89ee68b9c1d1154b4
Cameron Dale <camrdale@gmail.com>
2025-04-09 09:53:32 -0700

    2022 Day 9

---
+++ year2022/day9/part1.py
@@ -0,0 +1,52 @@
+from aoc.input import InputParser
+from aoc import log
+from aoc.map import Coordinate, Offset, NEIGHBORS, DIAGONAL_NEIGHBORS, UP, DOWN, LEFT, RIGHT
+from aoc.runner import Part
+
+
+WITHIN_ONE = set(NEIGHBORS + DIAGONAL_NEIGHBORS + [Offset(0,0)])
+
+DIRECTIONS = {'U': UP, 'R': RIGHT, 'D': DOWN, 'L': LEFT}
+
+
+def to_direction(offset: Offset) -> Offset:
+    return Offset(
+        0 if offset.x == 0 else offset.x // abs(offset.x),
+        0 if offset.y == 0 else offset.y // abs(offset.y))
+
+
+class Part1(Part):
+    def run(self, parser: InputParser) -> int:
+        input = parser.get_input()
+
+        head = Coordinate(0,0)
+        tail = head
+        visited = set([tail])
+        for line in input:
+            direction = DIRECTIONS[line.split()[0]]
+            length = int(line.split()[1])
+            for _ in range(length):
+                head = head.add(direction)
+                to_head = head.difference(tail)
+                if to_head not in WITHIN_ONE:
+                    tail = tail.add(to_direction(to_head))
+                    visited.add(tail)
+
+        log.log(log.RESULT, f'The number of positions visited by the tail: {len(visited)}')
+        return len(visited)
+
+
+part = Part1()
+
+part.add_result(13, r"""
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
+""")
+
+part.add_result(6498)
---
+++ year2022/day9/part2.py
@@ -0,0 +1,64 @@
+from aoc.input import InputParser
+from aoc import log
+from aoc.map import Coordinate, Offset, NEIGHBORS, DIAGONAL_NEIGHBORS, UP, DOWN, LEFT, RIGHT
+from aoc.runner import Part
+
+
+WITHIN_ONE = set(NEIGHBORS + DIAGONAL_NEIGHBORS + [Offset(0,0)])
+
+DIRECTIONS = {'U': UP, 'R': RIGHT, 'D': DOWN, 'L': LEFT}
+
+
+def to_direction(offset: Offset) -> Offset:
+    return Offset(
+        0 if offset.x == 0 else offset.x // abs(offset.x),
+        0 if offset.y == 0 else offset.y // abs(offset.y))
+
+
+class Part2(Part):
+    def run(self, parser: InputParser) -> int:
+        input = parser.get_input()
+
+        knots = [Coordinate(0,0)]*10
+        visited = set([knots[9]])
+        for line in input:
+            direction = DIRECTIONS[line.split()[0]]
+            length = int(line.split()[1])
+            for _ in range(length):
+                knots[0] = knots[0].add(direction)
+                for knot in range(1, 10):
+                    to_knot_ahead = knots[knot-1].difference(knots[knot])
+                    if to_knot_ahead not in WITHIN_ONE:
+                        knots[knot] = knots[knot].add(to_direction(to_knot_ahead))
+                        if knot == 9:
+                            visited.add(knots[9])
+
+        log.log(log.RESULT, f'The number of positions visited by the tail: {len(visited)}')
+        return len(visited)
+
+
+part = Part2()
+
+part.add_result(1, r"""
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
+""")
+
+part.add_result(36, r"""
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+""")
+
+part.add_result(2531)
