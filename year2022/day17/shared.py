dc0bd352a4eafecf9f912c06ec757b64811d9781
Cameron Dale <camrdale@gmail.com>
2025-04-18 09:09:23 -0700

    Improve 2022 Day 17 with cycle detection

--- year2022/day17/shared.py
+++ year2022/day17/shared.py
@@ -32,7 +32,6 @@ class TetrisMap(EmptyMap):
         self.next_jet = 0
         self.highest_rock = -1
         self.rock_num = 0
-        self.repeating_rock_index = None
     
     def get_jet(self) -> Offset:
         jet = JETS[self.jet_pattern[self.next_jet]]
@@ -65,37 +64,46 @@ class TetrisMap(EmptyMap):
             self.add_feature(ROCK, coord)
 
     def simulate(self, num_rocks: int) -> int:
-        while self.rock_num < num_rocks:
+        total_rocks = self.rock_num + num_rocks
+        while self.rock_num < total_rocks:
             self.max_y = self.highest_rock + 10
             rock = ROCKS[self.rock_num % len(ROCKS)].add(Offset(0, self.highest_rock + 4))
             self.drop_rock(rock)
             self.rock_num += 1
         return self.highest_rock + 1
+    
+    def find_cycle(self) -> tuple[tuple[int, int], tuple[int, int]]:
+        # (rock index, next jet, hash of top 25 lines of map): (rock num, highest rock)
+        results: dict[tuple[int, int, int], tuple[int, int]] = {}
+
+        # Skip a bit until the map is higher than 25
+        while self.simulate(1) < 25:
+            pass
+
+        while True:
+            height = self.simulate(1)
+            state = (self.rock_num % len(ROCKS), self.next_jet, hash(self.print_top_of_map(25)))
+            if state in results:
+                return results[state], (self.rock_num, height)
+            results[state] = (self.rock_num, height)
 
     def simulate_by_repetition(self, num_rocks: int) -> int:
-        # Numbers here work for the input.txt file, and were found by manual cycle detection
-        # at times when the input file repeats.
-        assert num_rocks > 1709 + 1705
+        (rock_num_start, height_start), (rock_num_end, height_end) = self.find_cycle()
+        cycle_length = rock_num_end - rock_num_start
+        cycle_height_change = height_end - height_start
+        log.log(log.INFO, f'Found a cycle of length {cycle_length} ({rock_num_start}-{rock_num_end}) that adds {cycle_height_change} to the height')
 
-        height_at_1709 = self.simulate(1709)
-        log.log(log.INFO, f'After 1709 rocks, the height is: {height_at_1709}')
-        log.log(log.DEBUG, self.print_top_of_map(20))
+        assert num_rocks > self.rock_num
 
-        height_at_1709_plus_1705 = self.simulate(1709 + 1705)
-        height_increase_every_1705 = height_at_1709_plus_1705 - height_at_1709
-        log.log(log.INFO, f'After 1709+1705={1709+1705} rocks, the height is {height_at_1709_plus_1705}, an increase of {height_increase_every_1705}')
-        log.log(log.DEBUG, self.print_top_of_map(20))
+        num_cycle_repetitions = (num_rocks - self.rock_num) // cycle_length
+        height_increase_for_repetitions = cycle_height_change * num_cycle_repetitions
+        rocks_remaining = (num_rocks - self.rock_num) % cycle_length
+        log.log(log.INFO, f'To get to {num_rocks} will require {num_cycle_repetitions} additional repetitions of the {cycle_length} rocks, adding {height_increase_for_repetitions} to the height')
 
-        num_repetitions_of_1705 = (num_rocks - 1709 - 1705) // 1705
-        height_increase_for_1705_repetitions = height_increase_every_1705 * num_repetitions_of_1705
-        rocks_remaining = (num_rocks - 1709 - 1705) % 1705
-        log.log(log.INFO, f'To get to {num_rocks} will require {num_repetitions_of_1705} additional repetitions of the 1705 rocks, adding {height_increase_for_1705_repetitions} to the height')
-
-        height_increase_for_rocks_remaining = self.simulate(1709 + 1705 + rocks_remaining) - height_at_1709_plus_1705
+        height_increase_for_rocks_remaining = self.simulate(rocks_remaining) - height_end
         log.log(log.INFO, f'The remaining {rocks_remaining} to get to {num_rocks} add {height_increase_for_rocks_remaining} to the height')
-        log.log(log.DEBUG, self.print_top_of_map(20))
 
-        return height_at_1709_plus_1705 + height_increase_for_1705_repetitions + height_increase_for_rocks_remaining
+        return height_end + height_increase_for_repetitions + height_increase_for_rocks_remaining
     
     def print_map(
             self, 
